ffea21b0b322754c79143c89e08b2cf1f3187758
Bazooka Bob <smythst@gmail.com>
2023-08-17 15:36:28 +0100

    Added main game loop, with exit on win.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
+extern crate rand;
+
+use std::cmp::Ordering;
 use std::io;
+use rand::Rng;
 
 fn main() {
-    println!("Guess the number");
-    println!("Please input your guess");
-    
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    println!("\nGuess the number between 1 and 100!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is {}", secret_number);
 
-    println!("You guessed: {}", guess);
+    loop{
+        println!("Please input your guess");
+        
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number");
+        println!("You guessed : {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
+}
 
 }
